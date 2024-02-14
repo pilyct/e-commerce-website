4e4e1099e0e1ed11a3dfdbe0c3596265d6ccf550
PilySwatch <pilar.cuellarta@gmail.com>
2024-02-14 17:48:08 +0100

    feat: add styles & Header component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './components/partials/header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: HeaderComponent },
+  { path: 'dashboard', component: HeaderComponent },
+  { path: 'profile', component: HeaderComponent },
+  { path: 'orders', component: HeaderComponent },
+  { path: 'cart', component: HeaderComponent },
+];
